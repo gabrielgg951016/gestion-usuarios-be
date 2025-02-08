3ee3e76c27e988ff430ebc46c3b3b85f41c3fbf9
Gabriel Garcia Gonzalez <ggg951016@gmail.com>
2025-02-07 21:19:06 -0600

    Initial Scaffolding

---
+++ src/main/java/com/xideral/gestion_usuarios_be/GestionUsuariosBeApplication.java
@@ -0,0 +1,13 @@
+package com.xideral.gestion_usuarios_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionUsuariosBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionUsuariosBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xideral/gestion_usuarios_be/GestionUsuariosBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xideral.gestion_usuarios_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionUsuariosBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
